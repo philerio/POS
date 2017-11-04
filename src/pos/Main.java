936d8e45f331f982bd966ff5cccc42c47443e68b
Myles <mylesandre1124@gmail.com>
2017-11-04 18:44:02 -0400

    main title fix

--- src/pos/Main.java
+++ src/pos/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("posmain.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Point Of Sale");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
